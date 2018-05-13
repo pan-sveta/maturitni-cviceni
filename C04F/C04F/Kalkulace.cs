929c11b4060451c97dc6e13cb34e9199e19353f2
Filip Štěpánek <stepafil@gmail.com>
2018-05-13 17:02:14 +0200

    C04F finished

---
+++ C04F/C04F/Kalkulace.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C04F
+{
+    class Kalkulace
+    {
+        //Metoda musí být statická aby jsme nemuseli třídu instancovat!
+        public static int ZadejCislo(string zprava, int dolniMez, int horniMez)
+        {
+            int cislo;
+            bool chyba;
+            
+            //"Pokus se převést text na číslo, do boolu chyba napiš zda-li se to povedlo a pokud ano vyhoď to do "cislo""
+            chyba = !int.TryParse(zprava, out cislo);
+
+            //Otestujeme, zda-li je číslo ve stanivených mezích
+            if (cislo > horniMez || cislo < dolniMez)
+            {
+                chyba = true;
+            }
+
+            //V případě že nstane chyba vyhoď vyjímku
+
+            if (chyba)
+            {
+                throw new Exception("Chybný formát vstupního řetězce.");
+            }
+
+            //Vrať číslo
+            return cislo;
+        }
+    }
+}
---
+++ C04F/C04F/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C04F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vytvoříme nekonečnou smyšku
+            do
+            {
+                //Potychytáváme chybu, kterou nám v případě chybně zadaného formátu vyhodí metoda v třídě Kalkulace
+                try
+                {
+                    Console.Write("Zadejte vstup [n t]: ");
+                    string mujText = Console.ReadLine();
+
+                    //Rozdělíme textový řetězec do pole na jednotlivé čísla stále v podobě textu
+                    string[] cisla = mujText.Split(' ');
+
+                    //Pomocí metody ZadejCislo převedeme jednotlivé položky v poli na čísla a načteme do proměné
+                    int n = Kalkulace.ZadejCislo(cisla[0], 0, 1000);
+                    int t = Kalkulace.ZadejCislo(cisla[1], 0, 12);
+
+                    //V zásadě zjistíme po přečtění zadání, že se každou hodinu počet bakterií zdvojnásobí
+                    //Načteme si proto první bakterie do proměné počet
+
+                    int count = n;
+
+                    //Nyní t-krát zdvojnásobíme počet
+
+                    for (int i = 0; i < t; i++)
+                    {
+                        count *= 2;
+                    }
+
+                    Console.WriteLine("Konečný počet bakterií je: "+count);
+                }
+                catch (Exception e)
+                {
+                    //V případě že nastane vyjímka vypíšeme ji
+                    Console.WriteLine(e.Message);
+                }
+            } while (true);
+        }
+    }
+}
