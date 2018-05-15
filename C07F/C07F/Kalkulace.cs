787bc8d9057568e77e335847bcf488dbda2eb440
Filip Štěpánek <stepafil@gmail.com>
2018-05-15 18:27:27 +0200

    C07F finished

---
+++ C07F/C07F/Kalkulace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07F
+{
+    class Kalkulace
+    {
+        //Metoda musí být statická aby jsme nemuseli třídu instancovat!
+        public static void ZadejN(string zprava, out int n)
+        {
+            string mujText;
+            bool chyba;
+
+            do
+            {
+                Console.Write("Zadej {0}: ", zprava);
+                mujText = Console.ReadLine();
+
+                //"Pokus se převést text na číslo, do boolu chyba napiš zda-li se to povedlo a pokud ano vyhoď to do "cislo""
+                chyba = !int.TryParse(mujText, out n);
+
+                //Ošetříme menší hodnoty
+                if (n < 3)
+                {
+                    chyba = true;
+                }
+            } while (chyba);
+        }
+    }
+}
---
+++ C07F/C07F/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C07F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Založíme proměnou pro počet pokusů
+            int n;
+
+            //Načteme počet pokusů metodou z třídy kalkulace
+            Kalkulace.ZadejN("počet hodů",out n);
+
+            //Vytvoříme si instanci náhodného generátoru
+            Random rand = new Random();
+
+            //Založíme si int pole o velikosti 6
+            //Je to ale pole tudíž indexi bude mít 0-5 to je dobré si uvědomit
+            int[] hody = new int[6];
+
+            //Uděláme cyklus, který se bude opakovat 7-krát
+            for (int i = 0; i < n; i++)
+            {
+                //Tady využijeme tríček
+                //rand.Next(0,6) nám bude náhodně generovat čísla 0-5, což je pro nás velmi výhodné => odpovídá to indexům pole
+                //Hodnotu v poli na tomto indexu pouze zvětšíme o jeden pomocí ++
+                hody[rand.Next(0,6)]++;
+            }
+
+            //Začneme vypisovat
+            Console.WriteLine("Pravděpodobnosti z {0} hodů vypadají následovně:",n);
+
+            //Mohli by jsme vše ručně vypisovat, ale jsme přece programátoři (haha, pokud tohle musíš číst tak fakt ne)! Použijeme cyklus
+            //Cyklus se bude opakovat 6 a i bude nabývat opět nám dobře známých hodnot 0-5
+            for (int i = 0; i < 6; i++)
+            {
+                //Pro číslo na kostce musíme ovšem přičíst 1 protože kostka nemá čísla 0-5
+                //Pomocí poměru hodů daného čísla a všech hodů vynásobeného stem získáme procetní poměr
+                //inty musíme přetypovat před dělením na double aby nám dali desetinné číslo!
+                Console.WriteLine("Číslo {0} má právděpodobnost {1}%.",i+1,((double)hody[i]/(double)n)*100);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
