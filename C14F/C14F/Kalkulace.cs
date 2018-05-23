7f2bfc5d3aae564e0d58b452efe4bee2852ce615
Filip Štěpánek <stepafil@gmail.com>
2018-05-23 20:40:30 +0200

    C14F finished

---
+++ C14F/C14F/Kalkulace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C14F
+{
+    class Kalkulace
+    {
+        //Metoda 1
+        //Parametr typu ref - "nasaje" proměnou do metody a upravuje její hodnotu, úprava se objeví i vně tělo metody
+        public static void Metoda(ref int vysledek, int cislo1, int cislo2, int cislo3, int cislo4)
+        {
+            vysledek += cislo1 + cislo2 + cislo3 + cislo4;
+        }
+
+        //Metoda 2
+        //Parametr typu out - "vyhazuje" parametr, který funguje stejně jako return
+        //Výhoda - Z metody můžeme vracet i více než jednu proměnou
+        public static void Metoda(out int vysledek, int cislo1, int cislo2, int cislo3)
+        {
+            vysledek = cislo1 + cislo2 + cislo3;
+        }
+
+        //Metoda 3ab
+        //Klasická emtoda vracející součet
+        public static int Metoda(int cislo1, int cislo2, int cislo3)
+        {
+            return cislo1 + cislo2 + cislo3;
+        }
+
+        //Metoda 4
+        //Params - Neomezený počet; Při volání metody můžeme nastavit libovolný počet parametrů,
+        // se kterými v tělu metody pracujeme jako s polem
+        public static int Metoda(params int[] cisla)
+        {
+            int vysledek = 0;
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                vysledek += cisla[i];
+            }
+
+            return vysledek;
+        }
+
+        //Metoda 5
+        //Volitelné parametry - Parametry kterým v "deklaraci" přiřadíme hodnoty jsou při volání nepoviné
+        //Pakliže je nezadefinujeme mají výchozí hodnotu (tu za =)
+        public static int Metoda(int cislo1 = 10, int cislo2 = 20, int cislo3 = 30, int cislo4 = 40, int cislo5=50)
+        {
+            return cislo1 + cislo2 + cislo3+cislo4+cislo5;
+        }
+}
+}
---
+++ C14F/C14F/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C14F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UPOZORNĚNÍ: 3a a 3b neodpovídají zadání, podle mě je v něm chyba a proto nedochází ke kýženým výsledkům
+            //Popis je v třídě
+
+            //Nadefinujeme si proměné
+            int cislo1 = 1, cislo2 = 2, cislo3 = 3, cislo4 = 4, cislo5 = 5, vysledek = 1000;
+
+            //Metoda 1
+            Console.WriteLine("Metoda 1:");
+            Kalkulace.Metoda(ref vysledek, cislo1, cislo2, cislo3, cislo4);
+            Console.WriteLine(vysledek);
+
+            //Restartujeme proměné na výchozí hodnoty (není nutné dělat všude a vždy ale pro zjednodušení to dělám všude a vždy)
+            cislo1 = 1; cislo2 = 2; cislo3 = 3; cislo4 = 4; cislo5 = 5; vysledek = 1000;
+
+            //Metoda 2
+            Console.WriteLine("Metoda 2:");
+            Kalkulace.Metoda(out vysledek, cislo1, cislo2, cislo3);
+            Console.WriteLine(vysledek);
+
+            //Restartujeme proměné na výchozí hodnoty (není nutné dělat všude a vždy ale pro zjednodušení to dělám všude a vždy)
+            cislo1 = 1; cislo2 = 2; cislo3 = 3; cislo4 = 4; cislo5 = 5; vysledek = 1000;
+
+            //Metoda 3a
+            Console.WriteLine("Metoda 3a:");
+            vysledek = Kalkulace.Metoda(cislo1, cislo2, cislo3);
+            Console.WriteLine(vysledek);
+
+            //Restartujeme proměné na výchozí hodnoty (není nutné dělat všude a vždy ale pro zjednodušení to dělám všude a vždy)
+            cislo1 = 1; cislo2 = 2; cislo3 = 3; cislo4 = 4; cislo5 = 5; vysledek = 1000;
+
+            //Metoda 3a
+            //Pojmenované parametry - Můžeme je dávat za přeskáčku pakliže jedadáváme ve tvaru jméno parametru:proměná
+            Console.WriteLine("Metoda 3b:");
+            vysledek = Kalkulace.Metoda(cislo3:cislo3*10,cislo2:cislo2*10,cislo1:cislo1*10);
+            Console.WriteLine(vysledek);
+
+            //Restartujeme proměné na výchozí hodnoty (není nutné dělat všude a vždy ale pro zjednodušení to dělám všude a vždy)
+            cislo1 = 1; cislo2 = 2; cislo3 = 3; cislo4 = 4; cislo5 = 5; vysledek = 1000;
+
+            //Metoda 4
+            Console.WriteLine("Metoda 4:");
+            vysledek = Kalkulace.Metoda(cislo1,cislo2,cislo3,cislo4,cislo5);
+            Console.WriteLine(vysledek);
+
+            //Restartujeme proměné na výchozí hodnoty (není nutné dělat všude a vždy ale pro zjednodušení to dělám všude a vždy)
+            cislo1 = 1; cislo2 = 2; cislo3 = 3; cislo4 = 4; cislo5 = 5; vysledek = 1000;
+
+            //Metoda 5
+            Console.WriteLine("Metoda 5:");
+            vysledek = Kalkulace.Metoda();
+            Console.WriteLine(vysledek);
+
+            Console.ReadLine();
+        }
+    }
+}
