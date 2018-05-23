b51f13f39c4b6270b19c818bf140635510a4b650
Filip Štěpánek <stepafil@gmail.com>
2018-05-23 19:55:07 +0200

    C08F finished

---
+++ C08F/C08F/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C08F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Založíme si List do kterého budeme ukládat objekty "Zavodnik"
+            //Pojmenujeme si ji závodníci
+            List<Zavodnik> zavodnici = new List<Zavodnik>();
+
+            //Uděláme si nekonečný cyklus
+            do
+            {
+                Console.Write("Zadej jméno závodníka: ");
+
+                string jmeno = Console.ReadLine();
+
+                //Když námi načtená proměná jmeno bude obsahovat slovo "KONEC"
+                //ToUpper() převede všechny písmená na velká tzn. Konec=>KONEC
+                if (jmeno.ToUpper()=="KONEC")
+                {
+                    //Přerušení našeho cyklu (kód bude pokračovat "pod ním")
+                    break;
+                }
+
+                //Instancujeme si třídu Zavodnik a do konstruktoru pošleme námni načtenou proměnou jmeno
+                Zavodnik zavodnik = new Zavodnik(jmeno);
+
+                //Přidáme instancovaný objekt do listu se závodníky
+                zavodnici.Add(zavodnik);
+            } while (true);
+
+            //Prázdnmý nový řádek
+            Console.WriteLine();
+
+            //"Projedeme" všechny závodníky v listu (ne ve smyslu pohlavního styku)
+            //Při každém průchodu cyklu se nám daný závodník uloží do proměné zavodnik
+            foreach (Zavodnik zavodnik in zavodnici)
+            {
+                //Vypíšeme jeho jméno, viz. ToString() definice v objektu
+                Console.WriteLine(zavodnik.ToString());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ C08F/C08F/Zavodnik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C08F
+{
+    class Zavodnik
+    {
+        //Vlastnost objektu typu string
+        //Private set znamená že de nastavit POUZE zevnitř objektu
+        public string Jmeno { get; private set; }
+
+        //Konstruktor, metoda, která se volá při založení objektu
+        //Má parametr jmeno typu string, musí se vpylnit při zakládání objektu
+        public Zavodnik(string jmeno)
+        {
+            //Nastavíme vlastnost objektu Jmeno na vstupní parametr kostruktopru
+            Jmeno = jmeno;
+        }
+
+        //override = přepsání -> každý objekt má metodu ToString() my jí tímto přepíšeme aby vralea, co chceme my
+        //V našem případě vrací vlastnost Jmeno
+        public override string ToString()
+        {
+            return Jmeno;
+        }
+    }
+}
