03d706376fbc68c73151e69226e213ab7e9b7957
Filip Štěpánek <stepafil@gmail.com>
2018-05-31 20:00:06 +0200

    C18F finished (not commented)

---
+++ C18F/C18F/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C18F
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            Nastroje.KreslitCtverce(e.Graphics);
+        }
+    }
+}
---
+++ C18F/C18F/Nastroje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Musíme přidat using!
+using System.Windows.Forms;
+
+namespace C18F
+{
+    class Nastroje
+    {
+        public static void KreslitCtverce(Graphics g)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int offset = 50 * i;
+                //Asi to není nejelegatnější ale beat me
+                g.DrawRectangle(Pens.Black,5+offset,5+offset, 500 - 2*offset, 500 - 2*offset);
+            }
+        }
+    }
+}
