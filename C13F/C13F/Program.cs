4151d2fd6cb90bdea4efbe1a3d119f8033de89ee
Filip Štěpánek <stepafil@gmail.com>
2018-06-03 20:33:35 +0200

    C13F finished (not commented)

---
+++ C13F/C13F/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Using!!
+using System.IO;
+
+namespace C13F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter("soubor.txt");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    writer.WriteLine(i.ToString());
+                }
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Je nám líto, ale nastala chyba při vytváření souboru: {0}",e.Message);
+                throw;
+            }
+            
+            //Peek vrací ascii hodnotu následujícího charu aniž by ho zkonzumoval
+            //"Neposune reader"
+            if (File.Exists("soubor.txt"))
+            {
+                StreamReader reader = new StreamReader("soubor.txt");
+
+                while (reader.Peek() > -1)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Je nám líto, ale soubor.txt neexistuje :(");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
